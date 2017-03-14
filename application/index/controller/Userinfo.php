056f0da012bdef60ee035f5b395d047582a09a9e
ghoulkodo <1037047001@qq.com>
2017-03-14 16:58:18 +0800

    完成teacher详细信息展示

--- application/index/controller/Userinfo.php
+++ application/index/controller/Userinfo.php
@@ -9,25 +9,41 @@ class Userinfo extends Common
 {
     public function Userinfo()
     {
-        parent::_initialize();
+        /*parent::_initialize();
         //角色权限检查
         $role = Loader::model('Role')->getRole($this->uid);
         if ($role != 2){
             $this->error('权限不正确');
-        }
+        }*/
         $fixdata = input('post.');
-        $data = [
-            'fix_phone' =>$fixdata['fix_phone'],
-            'fix_qq'  =>$fixdata['fix_qq'],
-            'fix_add' =>$fixdata['fix_add'],
-            'fix_dizhi'=>$fixdata['fix_dizhi'],
-            'fix_xueyuan' =>$fixdata['fix_xueyuan'],
-            'fix_kemu'  =>$fixdata['fix_kemu'],
-            'fix_class' =>$fixdata['fix_class'],
-        ];
-        
-        //echo $result = json_encode(['11111111111111']);
-        //die;
+        /*$data = [
+            'fix_phone'     =>$fixdata['fix_phone'],
+            'fix_qq'        =>$fixdata['fix_qq'],
+            'fix_add'       =>$fixdata['fix_add'],
+            'fix_dizhi'     =>$fixdata['fix_dizhi'],
+            'fix_xueyuan'   =>$fixdata['fix_xueyuan'],
+            'fix_kemu'      =>$fixdata['fix_kemu'],
+            'fix_class'     =>$fixdata['fix_class'],
+        ];*/
+
+        $update = Db::table('think_user')
+                    ->where('id',$_SESSION['think']['uid'])
+                    ->update([
+                        'phone'    =>$fixdata['fix_phone'],
+                        'qq'       =>$fixdata['fix_qq'],
+                        'add'      =>$fixdata['fix_add'],
+                        'dizhi'    =>$fixdata['fix_dizhi'],
+                        'xueyuan'  =>$fixdata['fix_xueyuan'],
+                        'kemu'     =>$fixdata['fix_kemu'],
+                        'class'    =>$fixdata['fix_class'],
+                    ]);
+        if (!$update) {
+            echo $result = json_encode(['更新失败']);
+            die;
+        } else {
+            echo $result = json_encode(['更新成功']);
+            die;
+        }
     }
 }
 
