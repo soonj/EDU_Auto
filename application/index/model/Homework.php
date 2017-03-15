75255855489384640339c7c073ecf53694672e2e
ghoulkodo <1037047001@qq.com>
2017-03-15 20:50:54 +0800

    布置作业基本完成，差预览！

--- application/index/model/Homework.php
+++ application/index/model/Homework.php
@@ -20,23 +20,47 @@ class Homework extends Model
 
     public function setSubject($data)
     {
+        //Session::delete('workcacheid', 'think');
 
+        //判断这个老师有没有正在布置的作业！
+        if (!empty($_SESSION['think']['workcacheid'])) {
+            $cache = db('homeworkcache')
+                    ->where('uid',$_SESSION['think']['uid'])
+                    ->where('keyid', $_SESSION['think']['workcacheid'])
+                    ->select();
+            if ($cache) {
+                $homework = true;
+            } else {
+                $homework = false;
+            }
+        } else {
+            $homework = false;
+        }
+        
+
+        //取出数组中的题目类型和uid
+        $key = $data['subject_type'];
         $uid = $_SESSION['think']['uid'];
 
-        //取出数组中的题目类型
-        $key = $data['subject_type'];
-
         //如果session中没有缓存元素，说明没有近期没发布作业，重新
-        if (empty($_SESSION['think']['workcacheid'])) {
+        if (!$homework) {
+
+            //生成作业更新Id
+            $keyid = ceil($uid + (time() / 2));
 
             //如果是选择题，需要输出选项
             if ($key == 'xuanze') {
                 $new_timu = '<div class="table-responsive" id="xuanze"><h4>'.$data['pick_main'].'</h4><h4>'.$data['pick_1'].'</h4><h4>'.$data['pick_2'].'</h4><h4>'.$data['pick_3'].'</h4><h4>'.$data['pick_4'].'</h4></div>';
+                $send = ['uid' => $uid, $key => $new_timu, 'keyid' => $keyid, $key.'num' => 1];
+            //如果是时间，去掉题目数量字段
+            } elseif($key == 'title'){
+                $new_timu =$data['pick_main'];
+                $send = ['uid' => $uid, $key => $new_timu, 'keyid' => $keyid];
             } else {
                 $new_timu = '<div class="table-responsive" id="xuanze"><h4>'.$data['pick_main'].'</h4></div>';
+                $send = ['uid' => $uid, $key => $new_timu, 'keyid' => $keyid, $key.'num' => 1];
             }
-            $keyid = ceil($uid + (time() / 2));
-            $send = ['uid' => $uid, $key => $new_timu, 'keyid' => $keyid, $key.'num' => 1];
+            
             $result = db('homeworkcache')->insert($send);
             if ($result) {
                 Session::set('workcacheid' , $keyid);
@@ -45,32 +69,44 @@ class Homework extends Model
                 return false;
             }
         } else {
+            //条件查询
             $cache = db('homeworkcache')
                     ->where('uid',$_SESSION['think']['uid'])
                     ->where('keyid', $_SESSION['think']['workcacheid'])
                     ->select();
-            if (empty($cache[0][$key])) {
-                if ($key == 'xuanze') {
-                    $new_timu = '<div class="table-responsive" id="xuanze"><h4>'.$data['pick_main'].'</h4><h4>'.$data['pick_1'].'</h4><h4>'.$data['pick_2'].'</h4><h4>'.$data['pick_3'].'</h4><h4>'.$data['pick_4'].'</h4></div>';
-                } else {
-                    $new_timu = '<div class="table-responsive" id="xuanze"><h4>'.$data['pick_main'].'</h4></div>';
-                }
-            } else {
-                if ($key == 'xuanze') {
-                    $new_timu = $cache[0][$key].'<div class="table-responsive" id="xuanze"><h4>'.$data['pick_main'].'</h4><h4>'.$data['pick_1'].'</h4><h4>'.$data['pick_2'].'</h4><h4>'.$data['pick_3'].'</h4><h4>'.$data['pick_4'].'</h4></div>';
-                } else {
-                    $new_timu = $cache[0][$key].'<div class="table-responsive" id="xuanze"><h4>'.$data['pick_main'].'</h4></div>';
-                }
-            }
-            //获取题目数量，并在对应的项目上+1s
-            $string = $key.'num'
-            $num = $cache[0][$string] + 1;
 
             //更新一个新的缓存Id
             $keyid = ceil($uid + (time() / 2));
 
-            //处理更新数据
-            $newdata = ['uid' => $uid, $key => $new_timu, 'keyid' => $keyid, $key.'num' => $num];
+            //获取题目数量，并在对应的项目上+1s
+            if ($key <> 'title') {
+                $string = $key.'num';
+                $num = $cache[0][$string] + 1;
+            }
+
+            if (empty($cache[0][$key])) {
+                if ($key == 'xuanze') {
+                    $new_timu = '<div class="table-responsive" id="xuanze"><h4>'.$data['pick_main'].'</h4><h4>'.$data['pick_1'].'</h4><h4>'.$data['pick_2'].'</h4><h4>'.$data['pick_3'].'</h4><h4>'.$data['pick_4'].'</h4></div>';
+                    $newdata = ['uid' => $uid, $key => $new_timu, 'keyid' => $keyid, $key.'num' => $num];
+                } elseif ($key == 'title') {
+                    $new_timu =$data['pick_main'];
+                    $newdata = ['uid' => $uid, $key => $new_timu, 'keyid' => $keyid];
+                } else {
+                    $new_timu = '<div class="table-responsive" id="xuanze"><h4>'.$data['pick_main'].'</h4></div>';
+                    $newdata = ['uid' => $uid, $key => $new_timu, 'keyid' => $keyid, $key.'num' => $num];
+                }
+            } else {
+                if ($key == 'xuanze') {
+                    $new_timu = $cache[0][$key].'<div class="table-responsive" id="xuanze"><h4>'.$data['pick_main'].'</h4><h4>'.$data['pick_1'].'</h4><h4>'.$data['pick_2'].'</h4><h4>'.$data['pick_3'].'</h4><h4>'.$data['pick_4'].'</h4></div>';
+                    $newdata = ['uid' => $uid, $key => $new_timu, 'keyid' => $keyid, $key.'num' => $num];
+                } elseif($key == 'title') {
+                    $new_timu =$data['pick_main'];
+                    $newdata = ['uid' => $uid, $key => $new_timu, 'keyid' => $keyid];
+                } else {
+                    $new_timu = $cache[0][$key].'<div class="table-responsive" id="xuanze"><h4>'.$data['pick_main'].'</h4></div>';
+                    $newdata = ['uid' => $uid, $key => $new_timu, 'keyid' => $keyid, $key.'num' => $num];
+                }
+            }
 
             //执行更新语句
             $result = db('homeworkcache')
