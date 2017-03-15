0fb06db7e546d1bedabafb932dd41e2285971dce
SungChiang <tianye263@gmail.com>
2017-03-15 19:17:31 +0800

    学生控制器方法补全

--- application/index/controller/Stu.php
+++ application/index/controller/Stu.php
@@ -11,6 +11,7 @@ use think\Loader;
  */
 class Stu extends Common
 {
+    protected $notice;
     public function _initialize()
     {
         parent::_initialize();
@@ -19,6 +20,9 @@ class Stu extends Common
         if ($role != 0){
             $this->error('权限不正确');
         }
+
+        //此处为获取所有通知，类型为数组，数组中包含对应的对象`array(obj()->content)`
+        $this->notice = Loader::controller('Notice')->getNotices($this->uid);
     }
 
     public function index($uname , $func = null)
@@ -30,22 +34,45 @@ class Stu extends Common
         if (!is_null($func)){
             return $this->$func();
         }
-
+        $this->assign('notice', $this->notice);
         return $this->fetch('bgd_index');
     }
 
-    public function homework()
+    //查看学生作业
+    private function homework()
     {
-        $this->fetch('Homework');
+        $data = Loader::model('Homework')->getHomework($this->uid);
+        $this->fetch('homework' , $data);
     }
 
-    public function profile()
+    //提交完成作业
+    private function doHomework()
     {
-        $this->fetch('profile');
+        $data = input('post');
+        Loader::model('Homework')->doHomework($data);
     }
 
-    public function res()
+    //查看用户详情
+    protected function profile()
     {
+        $data = Loader::model('Profile')->getProfile($this->uid);
+        $this->assign('userinfo', $data);
+
+        $this->fetch('Fixinfo');
+    }
+
+    //修改用户详情
+    protected function setProfile()
+    {
+        $data = input('post');
+        Loader::model('Profile')->setProfile($data);
+    }
+
+    //查看用户资源
+    private function res()
+    {
+        $data = Loader::model('Res')->getRes($this->uid);
+        $this->assign('ures' , $data);
         $this->fetch('res');
     }
 }
