50a21080a83357b349dcb202d4737aca4de32e9c
SungChiang <tianye263@gmail.com>
2017-03-18 17:57:07 +0800

    网站管理员

---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,45 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Admin extends Controller
+{
+    //网站浏览/登录记录
+    public function Log()
+    {
+
+    }
+    //网站信息查看和设置
+    public function settings()
+    {
+
+    }
+    //最新上传
+    public function viewRes()
+    {
+
+    }
+    //以学年初始化学生
+    public function initStu()
+    {
+
+    }
+
+    //学生管理（对应教师管理）
+    public function manage()
+    {
+
+    }
+    //对教师发送通知
+    public function sendMessage()
+    {
+
+    }
+    //课程管理
+    public function course()
+    {
+
+    }
+
+}
---
+++ application/admin/controller/Auth.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+//管理员登录验证类
+class Auth extends Controller
+{
+
+}
